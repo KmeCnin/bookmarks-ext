1b8e98c6fce0552f186ab536948d7d003afd2eb3
Pierre <pierrechanel.gauthier@gmail.com>
2014-02-24 15:25:18 +0100

    Call to webservice

--- js/popup.js
+++ js/popup.js
@@ -1,86 +1,91 @@
 var tree = null;
 var currentFolder = null;
 $(document).ready(function() {
+    // Hard token
+    var token = '1251c7c2c9f48e2e8ff80a77c5ec103c';
     // Get global tree from database
-//    $.get("", function(data) {
-//	   
-//    });
+    $.ajax({
+	   url: 'http://dev.pierrechanel-gauthier.com/bookmarks-ws/folders?last=&token='+token
+    }).done(function(data) {
+	   tree = {'id': 0, 'name': '<i class="fa fa-home"></i>', 'folders': data.folders};
+//	   console.log(JSON.stringify(tree));
     // Fake
-    tree = {
-	   'id': 1,
-	   'name': '<i class="fa fa-home"></i>',
-	   'folders': [
-		  {
-			 'id': 2,
-			 'name': 'Sciences',
-			 'folders': [
-				{
-				    'id': 3,
-				    'name': 'Biologie',
-				    'folders': [
-					   {
-						  'id': 12,
-						  'name': 'Neurologie',
-						  'folders': []
-					   }
-				    ]
-				},
-				{
-				    'id': 4,
-				    'name': 'Astrophysique',
-				    'folders': [
-					   {
-						  'id': 8,
-						  'name': 'Cosmologie',
-						  'folders': [
-							 {
-								'id': 10,
-								'name': 'Système solaire',
-								'folders': []
-							 }
-						  ]
-					   },
-					   {
-						  'id': 9,
-						  'name': 'Trous noirs',
-						  'folders': []
-					   }
-				    ]
-				},
-				{
-				    'id': 5,
-				    'name': 'Mathématiques',
-				    'folders': []
-				}
-			 ]
-		  },
-		  {
-			 'id': 6,
-			 'name': 'Actualités',
-			 'folders': [
-				{
-				    'id': 11,
-				    'name': 'Monde',
-				    'folders': []
-				}
-			 ]
-		  },
-		  {
-			 'id': 7,
-			 'name': 'Bandes-dessinées',
-			 'folders': [
-				{
-				    'id': 13,
-				    'name': 'Comics',
-				    'folders': []
-				}
-			 ]
-		  }
-	   ]
-    };
-    currentFolder = {'id': 1, 'name': 'Mes Bookmarks'};
-    displayFolders();
-    displayBreadcrumb(getBreadcrumb(tree, currentFolder.id, []));
+//    tree = {
+//	   'id': 1,
+//	   'name': '<i class="fa fa-home"></i>',
+//	   'folders': [
+//		  {
+//			 'id': 2,
+//			 'name': 'Sciences',
+//			 'folders': [
+//				{
+//				    'id': 3,
+//				    'name': 'Biologie',
+//				    'folders': [
+//					   {
+//						  'id': 12,
+//						  'name': 'Neurologie',
+//						  'folders': []
+//					   }
+//				    ]
+//				},
+//				{
+//				    'id': 4,
+//				    'name': 'Astrophysique',
+//				    'folders': [
+//					   {
+//						  'id': 8,
+//						  'name': 'Cosmologie',
+//						  'folders': [
+//							 {
+//								'id': 10,
+//								'name': 'Système solaire',
+//								'folders': []
+//							 }
+//						  ]
+//					   },
+//					   {
+//						  'id': 9,
+//						  'name': 'Trous noirs',
+//						  'folders': []
+//					   }
+//				    ]
+//				},
+//				{
+//				    'id': 5,
+//				    'name': 'Mathématiques',
+//				    'folders': []
+//				}
+//			 ]
+//		  },
+//		  {
+//			 'id': 6,
+//			 'name': 'Actualités',
+//			 'folders': [
+//				{
+//				    'id': 11,
+//				    'name': 'Monde',
+//				    'folders': []
+//				}
+//			 ]
+//		  },
+//		  {
+//			 'id': 7,
+//			 'name': 'Bandes-dessinées',
+//			 'folders': [
+//				{
+//				    'id': 13,
+//				    'name': 'Comics',
+//				    'folders': []
+//				}
+//			 ]
+//		  }
+//	   ]
+//    };
+	   currentFolder = {'id': 0, 'name': '<i class="fa fa-home"></i>'};
+	   displayFolders();
+	   displayBreadcrumb(getBreadcrumb(tree, currentFolder.id, []));
+    });
     
     $(document).on("click", ".goFolder", function() { 
 	   currentFolder = {'id': parseInt($(this).attr('data-id')), 'name': $(this).html()};
@@ -101,7 +106,6 @@ function displayFolders() {
 
 function displayBreadcrumb(breadcrumb) {
     // Display the breadcrumb from root to current folder
-    console.log(breadcrumb);
     $('.breadcrumb').html('');
     for (var i = 0; i < breadcrumb.length; i++) {
 	   if (i === breadcrumb.length-1)
